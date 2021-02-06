1bbc3cb9261511481024ba5ff9759626b6836945
gogojjh <651650302@qq.com>
2021-02-06 11:48:10 +0800

    add depthmonoregularization

---
+++ esvo_core/include/esvo_core/core/DepthMonoRegularization.h
@@ -0,0 +1,31 @@
+#ifndef ESVO_CORE_CORE_DEPTHMONOREGULARIZATION_H
+#define ESVO_CORE_CORE_DEPTHMONOREGULARIZATION_H
+
+#include <esvo_core/container/DepthMap.h>
+#include <esvo_core/core/DepthProblem.h>
+#include <memory>
+namespace esvo_core
+{
+  using namespace container;
+  namespace core
+  {
+    class DepthMonoRegularization
+    {
+    public:
+      typedef std::shared_ptr<DepthMonoRegularization> Ptr;
+
+      DepthMonoRegularization(std::shared_ptr<DepthProblemConfig> &dpConfigPtr);
+      virtual ~DepthMonoRegularization();
+
+      void apply(DepthMap::Ptr &depthMapPtr);
+
+    private:
+      std::shared_ptr<DepthProblemConfig> dpConfigPtr_;
+      size_t _regularizationRadius;
+      size_t _regularizationMinNeighbours;
+      size_t _regularizationMinCloseNeighbours;
+    };
+  } // namespace core
+} // namespace esvo_core
+
+#endif //ESVO_CORE_CORE_DepthMonoRegularization_H
